8ae389f2e6bef51023b6112a0d6adf7536e2300f
eya-bbjaouahdou* <eyajaouahdou78@gmail.com>
2022-12-10 15:00:56 +0100

    cc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import {React , useState} from 'react'
+import TodoForm from './components/TodoForm';
+import Todo from './components/Todo';
+import "./App.css";
 
-function App() {
+const App = () => {
+  let [todos , setTodos] = useState([]);
+  const[todoToShow , setTodoToShow] = useState("all");
+  const[toggleAllComplete , setToggleAllComplete] = useState(true);
+  
+  const addTodo = (todo)=>{
+    setTodos([todo, ...todos]);
+  };
+  const handleDelete = (id)=>{
+    setTodos(todos.filter((todo)=>todo.id !== id))
+  };
+  const updateTodoToShow = (s)=>{
+    setTodoToShow(s);
+  };
+  const removeAllTodosThatAreComplete = () =>{
+    setTodos(todos.filter((todo)=> !todo.complete));
+  };
+  const toggleComplete = (id)=>{setTodos(
+    todos.map((todo)=>{
+      if(todo.id === id){
+     return{
+      ...todo ,
+      complete : !todo.complete
+     }
+      }else{
+        return todo ;
+      }
+    })
+  )};
+  if(todoToShow === "active"){
+    todos = todos.filter((todo)=> !todo.complete);
+  }else if (todoToShow === "complete"){
+    todos = todos.filter((todo)=> todo.complete);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <TodoForm onSubmit = {addTodo}/>
+      {
+        todos.map((todo)=>(
+          <Todo key={todo.id} todo={todo} onDelete={()=> handleDelete(todo.id)} toggleComplete={()=> toggleComplete(todo.id)}/>
+        ))
+      }
+      <div>
+        <button className='update-btn btn' onClick={()=> updateTodoToShow("All")}>All</button>
+        <button className='update-btn btn' onClick={()=> updateTodoToShow("Active")}>Active</button>
+        <button className='update-btn btn' onClick={()=> updateTodoToShow("Complete")}>Complete</button>
+
+      </div>
+      <button className='all-btn btn' onClick={removeAllTodosThatAreComplete}>
+        Remove all complete</button>
+      <button className='all-btn btn' onClick={()=>{
+        setTodos(
+          todos.map((todo)=>({
+            ...todo,
+            complete : toggleAllComplete,
+          }))
+        );
+        setToggleAllComplete(!toggleAllComplete);
+      }}>
+        Toggle all complete : true </button>
+
     </div>
   );
-}
+};
 
-export default App;
+export default App ;
+
